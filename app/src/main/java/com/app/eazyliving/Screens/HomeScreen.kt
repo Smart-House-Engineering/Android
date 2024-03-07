51e61d4e0d24a593991f7a3ead214444b7b2a5ef
Anazeb <anam.aurangzeb@gmail.com>
2024-03-07 14:11:54 +0100

    Modified the login screen so that the keypad does not overlap the text fields

--- app/src/main/java/com/app/eazyliving/Screens/HomeScreen.kt
+++ app/src/main/java/com/app/eazyliving/Screens/HomeScreen.kt
@@ -2,7 +2,10 @@ package com.app.eazyliving.Screens
 
 import android.os.Build
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -14,6 +17,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.app.eazyliving.model.UserPayload
 import com.app.eazyliving.network.Cookies.TokenManager
@@ -21,31 +25,41 @@ import com.google.gson.Gson
 
 @Composable
 fun HomeScreen(navController: NavController) {
-    val context = LocalContext.current // Use LocalContext.current to get the context
-    var payload by remember {  mutableStateOf<Pair<String, String>?>(null)  }
+    val context = LocalContext.current
+    var payload by remember { mutableStateOf<Pair<String, String>?>(null) }
 
     // This will run once when the composable is loaded
     LaunchedEffect(key1 = true) {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                val decodedData = TokenManager.getDecodedTokenData(context)
-                decodedData?.second?.let {
-                    val gson = Gson()
-                    val userPayload = gson.fromJson(it, UserPayload::class.java) // Parsing the JSON into UserPayload
-                    payload = Pair(userPayload.user.email, userPayload.user.role) // Extract email and role
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val decodedData = TokenManager.getDecodedTokenData(context)
+            decodedData?.second?.let {
+                val gson = Gson()
+                val userPayload =
+                    gson.fromJson(it, UserPayload::class.java) // Parsing the JSON into UserPayload
+                payload =
+                    Pair(userPayload.user.email, userPayload.user.role) // Extract email and role
+            }
+        } else {
+            // Handle older Android versions or alert the user
+        }
+    }
+    LazyColumn(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        item {
+            Spacer(modifier = Modifier.height(30.dp))
+                payload.let {
+                    // Display the user information if available
+                    if (it != null) {
+                        Text(
+                            text = "Welcome ${it.first}! Your role is ${it.second}.",
+                            style = MaterialTheme.typography.bodyMedium
+                        )
+                    }
                 }
-            } else {
-                // Handle older Android versions or alert the user
             }
         }
-
-    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-        payload.let {
-            // Display the user information if available
-            if (it != null) {
-                Text(text = "Welcome ${it.first}! Your role is ${it.second}.", style = MaterialTheme.typography.bodyMedium)
-            }
-        } ?: Text(text = "Welcome to the Home Screen!", style = MaterialTheme.typography.bodyMedium)
     }
-}
 
 
