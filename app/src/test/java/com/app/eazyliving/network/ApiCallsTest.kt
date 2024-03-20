26f7e29db2c964c5aa5a06d3341008c38521e12c
Simon Eriksson <simon_eriksson_90@outlook.com>
2024-03-20 11:12:04 +0100

    setting up unit test stuff

---
+++ app/src/test/java/com/app/eazyliving/network/ApiCallsTest.kt
@@ -0,0 +1,24 @@
+package com.app.eazyliving.network
+
+import org.junit.jupiter.api.Assertions.*
+
+import org.junit.jupiter.api.Test
+
+class ApiCallsTest {
+
+    @Test
+    fun login() {
+    }
+
+    @Test
+    fun logout() {
+    }
+
+    @Test
+    fun getSensors() {
+    }
+
+    @Test
+    fun updateSensors() {
+    }
+}
